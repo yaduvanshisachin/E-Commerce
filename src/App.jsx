bd9444733415a0807c17e58ce4fa2696f798c1dd
Sachin Yadav <sy164164@gmail.com>
2025-07-17 21:29:10 +0530

    Simple E-Commerce Web

---
+++ src/App.jsx
@@ -0,0 +1,94 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import Category from './component/Category'
+import axios from 'axios'
+
+function App() {
+  const [finalCategory, setFinalCategory] = useState([]);
+  const [finalProduct, setFinalProduct] = useState([]);
+  const [catName, setCatName] = useState('');
+
+  let getCategory = () => {
+    axios.get('https://dummyjson.com/products/category-list')
+      .then((res) => {
+        // console.log(res.data);
+        setFinalCategory(res.data)
+      })
+      .catch((error) => {
+        console.error('Error fetching categories:', error);
+      });
+  }
+
+  let getProduct = () => {
+    axios.get('https://dummyjson.com/products')
+      .then((res) => {
+        // console.log(res.data.products);
+        setFinalProduct(res.data.products);
+      })
+      .catch((error) => {
+        console.error('Error fetching products:', error);
+      });
+  }
+
+  let pItems = finalProduct.map((v, i) => {
+    return (
+      <ProductItem key={i} pdata={v}/>
+    )
+  })
+
+  useEffect(() => {
+    getCategory();
+    getProduct();
+    // console.log(finalCategory);
+  }, [])
+
+  useEffect(() => {
+    if(catName !== '') {
+      // console.log(catName);
+      axios.get(`https://dummyjson.com/products/category/${catName}`)
+      .then((res) => {
+        // console.log(res.data.products);
+        setFinalProduct(res.data.products)
+      })
+      .catch((error) => {
+        console.error('Error fetching categories:', error);
+      });
+    }
+  }, [catName])
+
+  return (
+    <>
+    <div className='py-[40px]'>
+      <div className='max-w-[1320px] mx-auto'>
+        <h1 className='text-center text-[40px] font-bold mb-[20px]'>Our Product</h1>
+        <div className='grid grid-cols-[30%_auto] gap-[20px]'>
+          <div className='rounded-[10px]'>
+            {/* {finalCategory.length} */}
+            <Category finalCategory={finalCategory} setCatName={setCatName}/>
+          </div>
+          <div className='grid grid-cols-3 gap-4'>
+            {/* <ProductItem /> */}
+            {finalProduct.length >= 1 ? pItems : 'No Product Found'}
+          </div>
+        </div>
+      </div>  
+    </div>
+      
+    </>
+  )
+}
+
+export default App
+
+function ProductItem({pdata}) {
+  return (
+    <div className='bg-white rounded-[10px] shadow-lg p-[20px] text-center cursor-pointer'>
+      <img src={pdata.thumbnail} alt="Product 1" className='w-[100] h-[220px] object-cover' />
+      <h3 className='text-[20px] font-semibold mt-[10px]'>{pdata.title}</h3>
+      <p className='text-gray-600'> Rs. {pdata.price}</p>
+    </div>
+  )
+}
+//'https://dummyjson.com/products'
+// 'https://dummyjson.com/products/category-list'
+// `https://dummyjson.com/products/category/${catName}`
---
+++ src/component/Category.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function Category({finalCategory, setCatName}) {
+    let cat = finalCategory.map((v,i) => {
+        return (
+            <li key={i} onClick={()=>setCatName(v)} className='p-[10px] hover:bg-[#f0f0f0] cursor-pointer font-serif'>{v}</li>
+        )
+    })
+
+  return (
+    <div>
+      <h3 className='text-[25px] font-[500] p-[10px]'>Product </h3>
+      <ul>
+        {/* <li className='p-[10px] hover:bg-[#f0f0f0] cursor-pointer font-serif'>Product 1</li>
+        <li className='p-[10px] hover:bg-[#f0f0f0] cursor-pointer font-serif'>Product 2</li>
+        <li className='p-[10px] hover:bg-[#f0f0f0] cursor-pointer font-serif'>Product 3</li>
+        <li className='p-[10px] hover:bg-[#f0f0f0] cursor-pointer font-serif'>Product 4</li>
+        <li className='p-[10px] hover:bg-[#f0f0f0] cursor-pointer font-serif'>Product 5</li> */}
+        {cat}
+      </ul>
+    </div>
+  )
+}
